b1b85ff494a6b31b0f9cb6470f7210e22e493956
Nathan Lee <esoodev@gmail.com>
2018-07-02 14:17:07 +0900

    File type, null file error handling. Correctly displaying JSON responses.

--- app/routes.js
+++ app/routes.js
@@ -1,17 +1,16 @@
 const uploader = require("./services/uploader")
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.get("/", (req, res) => {
     // You'll create your note here.
     res.send("Hello")
   })
 
   app.post("/upload", (req, res) => {
-    console.log(req)
     try {
-      uploader.upload(req, res, __dirname + "/files/uploads/img/before")
+      uploader.upload(req, res, __dirname + "/files/uploads/img/before", ["image"]);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   })
 }
--- app/services/uploader.js
+++ app/services/uploader.js
@@ -3,9 +3,7 @@ const formidable = require("formidable"),
   util = require("util"),
   fs = require("fs");
 
-var exports = (module.exports = {});
-
-exports.upload = function(req, res, dir) {
+function upload(req, res, dir, checkTypes) {
   // Check for trailing '/' in dir
   if (dir[dir.length - 1] != "/") {
     dir = dir + "/";
@@ -16,59 +14,108 @@ exports.upload = function(req, res, dir) {
     uploadDir: dir + "/temp/"
   });
 
-  form.parse(req, function(err, fields, files) {
-    if (!files.filetoupload.name) {
-      res.writeHead(400, { "content-type": "application/json" });
-      res.write(
-        JSON.stringify({
-          status_code: 400,
-          error: "File is not included."
-        })
-      );
-      res.end();
-      return;
-    }
+  form.parse(req, function (err, fields, files) {
 
-    if (err) {
-      res.writeHead(500, { "content-type": "application/json" });
-      res.write(
-        JSON.stringify({
-          status_code: 500,
-          error: err
-        })
-      );
-      res.end();
-      return;
-    }
+    if (!_uploadHandleError({
+        req: req,
+        res: res,
+        dir: dir,
+        checkTypes: checkTypes,
+        err: err,
+        fields: fields,
+        files: files
+      })) return;
 
     var oldpath = files.filetoupload.path;
     var newpath = dir + files.filetoupload.name;
-    // TODO check trailing '/' after dir.
 
-    fs.rename(oldpath, newpath, function(err) {
+    // File rename error handling.
+    fs.rename(oldpath, newpath, function (err) {
       if (err) {
-        res.writeHead(500, { "content-type": "application/json" });
-        res.write(
-          JSON.stringify({
-            status_code: 500,
-            error: err
-          })
-        );
-        res.end();
-        return;
+        throw JSON.stringify({
+          content_type: "application/json",
+          status_code: 500,
+          error: err
+        });
       }
-      
-      res.writeHead(200, { "content-type": "application/json" });
-      res.write(
-        JSON.stringify({
-          status_code: 200,
-          message: "File uploaded and moved!",
-          file_name: files.filetoupload.name,
-          inspect: util.inspect({ fields: fields, files: files })
-        })
-      );
-      res.end();
-      return;
     });
+    res.set({
+      "Content-Type": "application/json",
+    })
+    res.status(200).send(
+      JSON.stringify({
+        status_code: 200,
+        message: "File uploaded and moved!",
+        file_name: files.filetoupload.name,
+        inspect: {
+          fields: fields,
+          files: files
+        }
+      }));
   });
-};
+}
+
+function _uploadHandleError(data) {
+
+  let req = data.req;
+  let res = data.res;
+  let dir = data.dir;
+  let files = data.files;
+  let err = data.err;
+  let checkTypes = data.checkTypes;
+
+  /**
+   * Handle error when file name is null.
+   */
+  if (!files.filetoupload.name) {
+
+    res.set({
+      "Content-Type": "application/json",
+    })
+    res.status(400).send(
+      JSON.stringify({
+        status_code: 400,
+        message: "File is not included."
+      }));
+    return false;
+  }
+
+  /**
+   * Handle error when file type is invalid.
+   */
+  if (checkTypes) {
+    if (!_fileIsTypes(files.filetoupload, [checkTypes])) {
+      res.set({
+        "Content-Type": "application/json",
+      })
+      res.status(400).send(
+        JSON.stringify({
+          status_code: 400,
+          message: "File type is invalid."
+        }));
+      return false;
+    }
+  }
+
+  /**
+   * Misc error handling.
+   */
+  if (err) {
+    res.set({
+      "Content-Type": "application/json",
+    })
+    res.status(500).send(
+      JSON.stringify({
+        status_code: 500,
+        error: err
+      }));
+    return false;
+  }
+}
+
+function _fileIsTypes(filetoupload, types) {
+  let fileType = filetoupload.type
+  return types.includes(fileType.substr(0, fileType.indexOf('/')));
+}
+
+module.exports.upload = upload;
